5638b2ce652e66a6be2fa0c17684a3fd36859d50
AndreaRB11 <142330476+AndreaRB11@users.noreply.github.com>
2023-08-15 09:25:03 -0600

    PrimerTrabajo

---
+++ Generaciones de computadoras - Andrea.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main (int argc,char **argv)
+{
+setlocale(LC_ALL, "");
+
+	printf("          GENERACIONES DE COMPUTADORAS Y SUS CARACTERÍSTICAS\n"
+	"\n"
+	"1era generación - Década de 1940 a 1950:\n"
+	" -Válvulas de vacío\n"
+	" -Tamaño y costo\n"
+	" -Programación basada en lenguaje de máquina\n"
+	"\n"
+	"\n"
+	"2da generación - Década de 1950 a 1960:\n"
+	" -Transistores\n"
+	" -Tecnología de montaje\n"
+	" -Memoria más rápida\n"
+	"\n"
+	"\n"
+	"3era generación - Década de 1960 a 1970:\n"
+	" -Circuitos integrados\n"
+	" -Mayor velocidad y capacidad\n"
+	" -Uso de lenguajes de programación de alto nivel\n"
+	"\n"
+	"\n"
+	"4ta generación - Década de 1960 a 1970:\n"
+	" -Microprocesadores\n"
+	" -Computadoras personales\n"
+	" -Avances gráficos y multimedia\n"
+	"\n"
+	"\n"
+	"5ta generación - Década de 1980 en adelante:\n"
+	" -Inteligecia artificial (IA)\n"
+	" -Procesamiento paralelo y distribuido\n"
+	" -Interacción natural e interfaces de usuario avanzadas\n");
+	return 0;
+}
+
+
